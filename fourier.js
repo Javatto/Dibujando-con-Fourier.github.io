6c8d569b6d682bfdde98c142e7abd6c691569d01
Javatt <63569701+Javatto@users.noreply.github.com>
2022-06-15 14:11:32 -0500

    Reinicio de app

---
+++ fourier.js
@@ -0,0 +1,47 @@
+/*
+* Xk = sum desde n=0 a N-1 de (xn * (cos(2*PI*n*k/N) - i * sen(2*PI*n*k/N)))
+*                            señal   [(x1,y1),(x2,y2),(x3,y3)...]
+*                                     a+bi --> x1+i*y1
+* Xk --> arreglo de números complejos
+*/
+
+class Complejo {
+  constructor(a, b) {
+    this.re = a;
+    this.im = b;
+  }
+
+  add(c) {
+    this.re += c.re;
+    this.im += c.im;
+  }
+  // PEIU (a+bi)(c+di) = (ac-bd) + (ad+bc)i
+  // ac + adi + bci - bd
+  mult(c) {
+    const re = this.re * c.re - this.im * c.im;
+    const im = this.re * c.im + this.im * c.re;
+    return new Complejo(re, im);
+  }
+}
+
+// Arreglo de la señal
+function dft(x) {
+  const X = []; // arreglo de números complejos
+  const N = x.length;
+  for (let k = 0; k < N; k++) {
+    let sum = new Complejo(0, 0);
+    for (let n = 0; n < N; n++) {
+      const phi = (TWO_PI * k * n) / N;
+      const c = new Complejo(cos(phi), -sin(phi));
+      sum.add(x[n].mult(c));
+    }
+    sum.re = sum.re / N;
+    sum.im = sum.im / N;
+
+    let frec = k;
+    let amp = sqrt(sum.re * sum.re + sum.im * sum.im);
+    let fase = atan2(sum.im, sum.re);
+    X[k] = { re: sum.re, im: sum.im, frec, amp, fase };
+  }
+  return X;
+}
---
+++ sketch.js
@@ -0,0 +1,98 @@
+const USER = 0;
+const FOURIER = 1;
+
+let x = [];
+let fourierX;
+let tiempo = 0;
+let ruta = [];
+let dibujo = [];
+let state = -1;
+
+function mousePressed() {
+  state = USER;
+  dibujo = [];
+  x = [];
+  tiempo = 0;
+  ruta = [];
+}
+
+function mouseReleased() {
+  state = FOURIER;
+  const skip = 1;
+  for (let i = 0; i < dibujo.length; i += skip) {
+    x.push(new Complejo(dibujo[i].x, dibujo[i].y));
+  }
+  fourierX = dft(x);
+
+  fourierX.sort((a, b) => b.amp - a.amp);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background('#333');
+  fill(255);
+  textAlign(CENTER);
+  textSize(64);
+  text("¡Dibuja algo!", width/2, height/2);
+}
+
+function epiciclos(x, y, rotacion, fourier) {
+  for (let i = 0; i < fourier.length; i++) {
+    let prevx = x;
+    let prevy = y;
+    let frec = fourier[i].frec;
+    let radius = fourier[i].amp;
+    let fase = fourier[i].fase;
+    x += radius * cos(frec * tiempo + fase + rotacion);
+    y += radius * sin(frec * tiempo + fase + rotacion);
+
+    stroke(255, 100);
+    noFill();
+    ellipse(prevx, prevy, radius * 2);
+    stroke(255);
+    line(prevx, prevy, x, y);
+  }
+  return createVector(x, y);
+}
+
+function draw() {
+
+  if (state == USER) {
+  background('#333');
+    let punto = createVector(mouseX - width / 2, mouseY - height / 2);
+    
+    dibujo.push(punto);
+    stroke(255);
+    noFill();
+    beginShape();
+    for (let v of dibujo) {
+      vertex(v.x + width / 2, v.y + height / 2);
+    }
+    endShape();
+  } else if (state == FOURIER) {
+  background('#333');
+    let v = epiciclos(width / 2, height / 2, 0, fourierX);
+    ruta.push(v);
+    beginShape();
+    noFill();
+    strokeWeight(2);
+    stroke(255, 0, 255);
+    for (let i = 0; i < ruta.length; i++) {
+      vertex(ruta[i].x, ruta[i].y);
+    }
+    endShape();
+
+    const dt = TWO_PI / fourierX.length;
+    tiempo += dt;
+
+    console.log(fourierX.length);
+    
+
+    if (tiempo > TWO_PI) {
+      tiempo = 0;
+      ruta = [];
+      state = -1;
+    }
+  }
+
+}
